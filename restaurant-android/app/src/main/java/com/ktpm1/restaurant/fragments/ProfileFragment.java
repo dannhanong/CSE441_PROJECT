7a1671d79ec87bdc2a9060e85e6b67e030650b5f
Nguyen Ngoc Dang <gatapchoi1022@gmail.com>
2024-10-16 14:08:13 +0700

    chinh profile

--- restaurant-android/app/src/main/java/com/ktpm1/restaurant/fragments/ProfileFragment.java
+++ restaurant-android/app/src/main/java/com/ktpm1/restaurant/fragments/ProfileFragment.java
@@ -7,6 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ktpm1.restaurant.R;
 
@@ -19,7 +27,71 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.activity_profile, container, false);
+
+        ImageView profileImage = view.findViewById(R.id.profileImage);
+        TextView username = view.findViewById(R.id.username);
+        TextView email = view.findViewById(R.id.email);
+        ImageButton editProfileButton = view.findViewById(R.id.editProfileButton);
+        ListView settingsList = view.findViewById(R.id.settings_list);
+        Button logoutButton = view.findViewById(R.id.logout_button);
+
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Chỉnh sửa profile", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Đăng xuất", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        String[] settingsOptions = {
+                "Phương thức thanh toán",
+                "Giỏ hàng của tôi",
+                "Đổi mật khẩu",
+                "Trợ giúp & Báo cáo",
+                "Thông báo",
+                "Chính sách bảo mật",
+                "Tin tức & Dịch vụ"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, settingsOptions);
+        settingsList.setAdapter(adapter);
+
+        settingsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Toast.makeText(getActivity(), "Phương thức thanh toán", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(getActivity(), "Giỏ hàng của tôi", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(getActivity(), "Đổi mật khẩu", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        Toast.makeText(getActivity(), "Trợ giúp & Báo cáo", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 4:
+                        Toast.makeText(getActivity(), "Thông báo", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 5:
+                        Toast.makeText(getActivity(), "Chính sách bảo mật", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 6:
+                        Toast.makeText(getActivity(), "Tin tức & Dịch vụ", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+
+        return view;
     }
 }
